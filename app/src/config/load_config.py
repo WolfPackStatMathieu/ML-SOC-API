d23ffa27b76cd0d4413a9f1490da80a61244c1c7
mthomassin <mathieu.thomassin@eleve.ensai.fr>
2024-07-24 08:21:07 +0000

    add comments

--- app/src/config/load_config.py
+++ app/src/config/load_config.py
@@ -2,11 +2,11 @@
 Module for loading configuration from a YAML file.
 
 This module provides a function `load_config` to load configuration settings
-from a specified YAML file into a dictionary. If the file does not exist, 
+from a specified YAML file into a dictionary. If the file does not exist,
 it returns an empty dictionary.
 
 Imports:
-    os: Provides a way of using operating system dependent functionality like 
+    os: Provides a way of using operating system dependent functionality like
     reading or writing to the file system.
     yaml: Provides YAML parsing and emitting capabilities.
 
@@ -21,14 +21,14 @@ import yaml  # Module pour manipuler les fichiers YAML
 def load_config(config_path="config.yaml"):
     """
     Load the YAML configuration from the specified path.
-    
+
     Parameters:
     config_path (str): The path to the configuration file. Default is "config.yaml".
-    
+
     Returns:
     dict: The loaded configuration as a dictionary. Returns an
     empty dictionary if the file does not exist.
-    
+
     This function checks if the configuration file exists at the specified path.
     If it does, it opens the file, reads the YAML content, and loads it into a dictionary.
     If the file does not exist, it returns an empty dictionary.
@@ -36,6 +36,6 @@ def load_config(config_path="config.yaml"):
     config = {}  # Initialise un dictionnaire vide pour stocker la configuration
     if os.path.exists(config_path):  # Vérifie si le fichier de configuration existe
         with open(config_path, "r", encoding="utf-8") as file:  # Ouvre le fichier en lecture
-# avec encodage UTF-8
+            # avec encodage UTF-8
             config = yaml.safe_load(file)  # Charge le contenu YAML dans un dictionnaire
     return config  # Retourne le dictionnaire de configuration
